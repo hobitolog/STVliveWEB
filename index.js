727f177d7969630fc7d4ad789cd712f52e30b45d
kosert <robert.kosakowski@student.put.poznan.pl>
2017-07-05 17:06:45 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Kosert on 2017-07-04.
+ */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(80, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})
