77ba74cf01a8756da6ac70088eee4a968b74f29d
kosert <robert.kosakowski@student.put.poznan.pl>
2017-07-17 19:02:13 +0200

    backend do transmisji zastępczej

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ var cheerio = require('cheerio')
 var crypto = require('crypto')
 var qs = require('querystring')
 var exec = require('child_process').exec
+var execSync = require('child_process').execSync
 var app = express()
 
+// make all files in /public/ available globally
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 // saves settings to settings.json
-// if  reloadNginx=true => applies settings to nginx.conf
-function saveConfig( reloadNginx )
-{
+// if reloadNginx=true => applies settings to nginx.conf
+// if onStart=true => starts playing offline file, default=false
+function saveConfig( reloadNginx, onStart) {
+  onStart = onStart || false
   var json = JSON.stringify(settings)
   fs.writeFile('settings.json', json, 'utf8', function(err) {
     if(err) console.log(err)
@@ -33,7 +36,7 @@ function saveConfig( reloadNginx )
     var copy = true
     lineReader.on('line', function (line)
     {
-      if(line === "#FFMPEG_END" || line === "#HLS_VARIANT_END")
+      if(line === "#FFMPEG_END" || line === "#FFMPEG_OFFLINE_END" || line === "#HLS_VARIANT_END" || line === "#HLS_OFFLINE_VARIANT_END")
         copy = true
 
       if(copy) {
@@ -61,12 +64,42 @@ function saveConfig( reloadNginx )
               nginxConf.write(' -b:a ')
               nginxConf.write(q.audio.bitrate)
               nginxConf.write(' -tune zerolatency -preset veryfast -f flv ')
-              nginxConf.write('rtmp://localhost:1935/hls/live_')
+              nginxConf.write('rtmp://localhost:1935/hlsOnline/live_')
               nginxConf.write(q.name)
           }
         }
         nginxConf.write(' name=')
         nginxConf.write(settings.streamKey)
+        nginxConf.write(';\n')
+        copy = false
+      }
+
+      if(line === "#FFMPEG_OFFLINE_BEGIN")
+      {
+        nginxConf.write('exec ffmpeg -i rtmp://localhost:1935/offline/')
+        nginxConf.write(offlineStreamKey)
+        nginxConf.write(' -async 1 -vsync -1 ')
+
+        for (var i = 0; i < settings.qualities.length; i++) {
+          var q = settings.qualities[i]
+          if(q.selectedOffline) {
+              nginxConf.write('\n-c:v ')
+              nginxConf.write(q.video.codec)
+              nginxConf.write(' -vf scale=')
+              nginxConf.write(q.video.resolution.replace('x', ':'))
+              nginxConf.write(' -b:v ')
+              nginxConf.write(q.video.bitrate)
+              nginxConf.write(' -c:a ')
+              nginxConf.write(q.audio.codec)
+              nginxConf.write(' -b:a ')
+              nginxConf.write(q.audio.bitrate)
+              nginxConf.write(' -tune zerolatency -preset veryfast -f flv ')
+              nginxConf.write('rtmp://localhost:1935/hlsOffline/live_')
+              nginxConf.write(q.name)
+          }
+        }
+        nginxConf.write(' name=')
+        nginxConf.write(offlineStreamKey)
         nginxConf.write(';\n')
         copy = false
       }
@@ -89,24 +122,48 @@ function saveConfig( reloadNginx )
         copy = false
       }
 
+      if(line === "#HLS_OFFLINE_VARIANT_BEGIN")
+      {
+        for (var i = 0; i < settings.qualities.length; i++) {
+          var q = settings.qualities[i]
+          if(q.selectedOffline) {
+            nginxConf.write('hls_variant _')
+            nginxConf.write(q.name)
+            nginxConf.write(' BANDWIDTH=')
+            var totalBitrate = parseInt(q.video.bitrate)*1000 + parseInt(q.audio.bitrate)*1000
+            nginxConf.write(totalBitrate.toString())
+            nginxConf.write(',RESOLUTION=')
+            nginxConf.write(q.video.resolution)
+            nginxConf.write(';\n')
+          }
+        }
+        copy = false
+      }
     });
 
-    exec(path.join(settings.nginxPath, '/sbin/nginx -s reload'), function(e, stdout, stderr) {
-      if (e instanceof Error) {
-          console.error(e);
-          //throw e;
+    lineReader.on('close', function () {
+      try {
+            var output = execSync(path.join(settings.nginxPath, '/sbin/nginx -s reload'))
+      } catch (e) {
+        //console.log(e);
       }
-      console.log("Nginx.conf reloaded")
-     })
+      console.log("Nginx reloaded");
+      if (onStart) {
+        playOfflineFile()
+      }
+    })
+
   }
 }
 
-
 // loads settings from settings.json file
-function loadSettings()
-{
+function loadSettings(){
   var contents = fs.readFileSync('settings.json', 'utf8')
   settings = JSON.parse(contents)
+}
+
+function loadOfflineFiles() {
+  fileList = fs.readdirSync(path.join(__dirname, '/videos'))
 }
 
 function topSecretAuth(req) {
@@ -118,34 +175,122 @@ function topSecretAuth(req) {
   else return true
 }
 
-function getCheckBoxCode(quality) {
+// get input checkbox html code for specified quality
+function getCheckBoxCode(quality, advanced) {
   var part1 = '<input type="checkbox" name="quality" value="'
   var checked = quality.selected ? '" checked>' : '">'
 
-  var xd = '<input type="text" name="bitrate_video_' + quality.name + '" value="'
-
-  return part1.concat(quality.name, checked, quality.name, ' - video: ', quality.video.resolution, ', ', quality.video.codec,
-  ', bitrate: ', '<input type="text" name="bitrate_video_', quality.name, '" value="', quality.video.bitrate, '">',
-  ' audio: ', quality.audio.codec, ', bitrate: ', '<input type="text" name="bitrate_audio_', quality.name, '" value="', quality.audio.bitrate, '">','<br>')
+  if(advanced) {
+    return part1.concat(quality.name, checked, quality.name, ' - video: ', quality.video.resolution, ', ', quality.video.codec,
+    ', bitrate: ', '<input type="text" name="bitrate_video_', quality.name, '" value="', quality.video.bitrate, '">',
+    ' audio: ', quality.audio.codec, ', bitrate: ', '<input type="text" name="bitrate_audio_', quality.name, '" value="', quality.audio.bitrate, '">','<br>')
+  }
+  else {
+    return part1.concat(quality.name, checked, quality.name, ' |')
+  }
 }
 
+function playOfflineFile() {
+  if(fileList.length > 0 && settings.qualities.filter(x => x.selectedOffline).length > 0)
+  {
+    var f = fileList.shift()
+    fileList.push(f)
+    var fpath = path.join(__dirname, '/videos', f)
+    console.log('Now playing: ' + f);
+    var cmd = 'ffmpeg -re -i ' + fpath + ' -c copy -f flv rtmp://localhost:1935/offline/' + offlineStreamKey
+    exec(cmd, function(e, stdout, stderr){
+      if (e instanceof Error) {
+          console.error(e);
+        }
+        playOfflineFile()
+    })
+  }
+  else {
+    console.log(fileList);
+    console.log(settings.qualities.filter(x => x.selectedOffline));
+    console.log('[WARN] No offline files to play or no qualities specified.');
+  }
+}
+
+// serve index.html
 app.get('/', function (req, res) {
+  //TODO: chat
   res.sendFile(path.join(__dirname, '/html', '/index.html'))
 })
 
-app.get('/admin', function (req, res)
-{
+// serve admin panel
+app.get('/admin', function (req, res){
   if (topSecretAuth(req))
   {
     fs.readFile(path.join(__dirname, '/html', '/admin.html'),'utf8', function (err, html) {
         var $ = cheerio.load(html)
 
-        for (var i = 0; i < settings.qualities.length; i++) {
-          $('div[id=checkboxes]').append(getCheckBoxCode(settings.qualities[i]))
-        }
-        $('input[id=sk]').val(settings.streamKey)
-        res.send($.html())
+            if (fileList.length == 0) {
+               $('ul[id=offlineFiles]').append('Folder /videos jest pusty.')
+            } else {
+              for (var i = 0; i < fileList.length; i++) {
+                $('ul[id=offlineFiles]').append('<li>'.concat(fileList[i], '</li>'))
+              }
+            }
+            for (var i = 0; i < settings.qualities.length; i++) {
+              $('div[id=checkboxes]').append(getCheckBoxCode(settings.qualities[i], true))
+              $('div[id=offlineCheckboxes]').append(getCheckBoxCode(settings.qualities[i], false))
+            }
+            $('input[id=sk]').val(settings.streamKey)
+            res.send($.html())
+        })
+  }
+  else
+  {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="admin"')
+    res.end('Access denied')
+  }
+})
+
+app.post('/saveOfflineSettings', function (req, res) {
+  if (topSecretAuth(req))
+  {
+    var body = '';
+    req.on('data', function (data)
+    {
+       body += data;
+    });
+    req.on('end', function ()
+    {
+      var params = qs.parse(body)
+
+      for (var i = 0; i < settings.qualities.length; i++) {
+        settings.qualities[i].selectedOffline = false
+      }
+
+      for (var i = 0; i < params.quality.length; i++) {
+        if(Array.isArray(params.quality))
+          var q = settings.qualities.find(x => x.name === params.quality[i])
+        else
+          var q = settings.qualities.find(x => x.name === params.quality)
+        q.selectedOffline = true
+      }
+
+      saveConfig(true)
+      playOfflineFile()
     })
+    res.redirect('/admin')
+  }
+  else
+  {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="admin"')
+    res.end('Access denied')
+  }
+})
+
+app.post('/reloadOfflineFiles', function (req, res) {
+  if (topSecretAuth(req))
+  {
+    loadOfflineFiles()
+    playOfflineFile()
+    res.redirect('/admin')
   }
   else
   {
@@ -172,7 +317,10 @@ app.post('/saveQualitySettings', function (req, res) {
       }
 
       for (var i = 0; i < params.quality.length; i++) {
-        q = settings.qualities.find(x => x.name === params.quality[i])
+        if(Array.isArray(params.quality))
+          var q = settings.qualities.find(x => x.name === params.quality[i])
+        else
+          var q = settings.qualities.find(x => x.name === params.quality)
         q.selected = true
         var vid = 'bitrate_video_' + q.name
         var aud = 'bitrate_audio_' + q.name
@@ -229,23 +377,36 @@ app.post('/changePassword', function (req, res) {
   }
 })
 
-// load settings and start server
+// load settings
 loadSettings()
+//generate new random streamKey for offline files
+var offlineStreamKey = crypto.randomBytes(20).toString('hex')
+//load list of offline files
+loadOfflineFiles()
+//start server
 var server = app.listen(80, function () {
-var host = server.address().address
-var port = server.address().port
+  var host = server.address().address
+  var port = server.address().port
 
-console.log("Serwer OK, adres: http://%s:%s", host, port)
+  console.log("Serwer OK, adres: http://%s:%s", host, port)
 
-// stop nginx (if it's running)
-exec(path.join(settings.nginxPath, '/sbin/nginx -s stop'), function(e, stdout, stderr){})
-
-// start nginx
-exec(path.join(settings.nginxPath, '/sbin/nginx'), function(e, stdout, stderr) {
-  if (e instanceof Error) {
-      console.error(e);
-      throw e;
+  // if nginx is running
+  if(fs.existsSync(path.join(settings.nginxPath, '/logs/nginx.pid')))
+  {
+    console.log("Nginx already running");
+    //save settings from JSON to nginx.conf
+    saveConfig(true, true)
   }
-  console.log("Nginx OK")
-})
+  else
+  {
+    // start nginx
+    exec(path.join(settings.nginxPath, '/sbin/nginx'), function(e, stdout, stderr) {
+      if (e instanceof Error) {
+          console.error(e);
+      }
+      console.log("Nginx OK")
+      //save settings from JSON to nginx.conf
+      saveConfig(true, true)
+    })
+  }
 })
--- public/main.js
+++ public/main.js
@@ -1,35 +1,36 @@
-var liveSource = 'http://192.168.56.101:8081/hls/live.m3u8'
+var liveSource = 'http://192.168.56.101:8081/hlsOnline/live.m3u8'
+var offlineSource = 'http://192.168.56.101:8081/hlsOffline/live.m3u8'
 
 if(Hls.isSupported())
 {
   console.log("HLS supported");
   var player = document.getElementById('player')
-  var hls = new Hls()
-  hls.attachMedia(player)
-  hls.loadSource(liveSource)
+  var hlsOnline = new Hls()
+  hlsOnline.attachMedia(player)
+  hlsOnline.loadSource(liveSource)
 
-  hls.on(Hls.Events.ERROR, function (event, data) {
+  hlsOnline.on(Hls.Events.ERROR, function (event, data) {
     console.log(data);
     if (data.fatal) {
       switch(data.type) {
       case Hls.ErrorTypes.NETWORK_ERROR:
         // try to recover network error
         console.log("liveStream: fatal network error encountered");//, trying to recover...");
-        hls.loadSource(liveSource)
+        hlsOnline.loadSource(liveSource)
         break;
       case Hls.ErrorTypes.MEDIA_ERROR:
-        console.log("fatal media error encountered, trying to recover...");
-        hls.recoverMediaError();
+        console.log("liveStream: fatal media error encountered, trying to recover...");
+        hlsOnline.recoverMediaError();
         break;
       default:
         // cannot recover
-        hls.destroy();
+        hlsOnline.destroy();
         break;
       }
     }
   });
 
-  hls.on(Hls.Events.MANIFEST_PARSED,function(event, data) {
+  hlsOnline.on(Hls.Events.MANIFEST_PARSED,function(event, data) {
     player.play();
     for (var i = 0; i < data.levels.length; i++) {
       q = data.levels[i]
@@ -42,9 +43,9 @@ if(Hls.isSupported())
 function selectQuality()
 {
   var selected = $("#qualitySelect")[0].value
-  if( selected === "auto") hls.currentLevel = -1;
+  if( selected === "auto") hlsOnline.currentLevel = -1;
   else {
-    hls.currentLevel = selected;
+    hlsOnline.currentLevel = selected;
   }
-  console.log(hls.currentLevel);
+  console.log(hlsOnline.currentLevel);
 }
